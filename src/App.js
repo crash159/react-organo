597840cd3bfb821f326ba92458c547e08b59ce8e
Gabriel Felippe <gabrielalexfelippe@gmail.com>
2023-07-19 15:56:38 -0300

    Parity Aula

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Time from "./componentes/Time";
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState([
     {
       nome: 'Programação',
       corPrimaria: '#D9F7E9',
@@ -42,7 +42,7 @@ function App() {
       corPrimaria: '#FFEEDF',
       corSecundaria: '#FF8A29'
     },
-  ]
+  ])
 
   const inicial = [
     {
@@ -197,6 +197,15 @@ function App() {
     console.log('Deletando Colaborador...')
   }
 
+  function mudarCorDoTime(cor, nome) {
+    setTimes(times.map(time => {
+      if (time.nome === nome) {
+        time.corSecundaria = cor
+      }
+      return time;
+    }));
+  }
+
   return (
     <div>
       <Banner />
@@ -207,6 +216,7 @@ function App() {
           <Time key={indice} 
             time={time} 
             colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} 
+            mudarCor={mudarCorDoTime}
             aoDeletar={deletarColaborador}
           />
         )}
