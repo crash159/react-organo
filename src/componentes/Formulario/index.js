bbbf3c534d2b64d4d87141053cf680764475ce88
Gabriel Felippe <gabrielalexfelippe@gmail.com>
2023-11-28 15:13:53 -0300

    Update Styling

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,78 +4,78 @@ import Campo from '../Campo'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
-const Formulario = ({aoCadastrar, times, cadastrarTime}) => {
+const Formulario = ({ aoCadastrar, times, cadastrarTime }) => {
 
-    const [nome, setNome] = useState('')
-    const [cargo, setCargo] = useState('')
-    const [imagem, setImagem] = useState('')
-    const [time, setTime] = useState('')
-    const [nomeTime, setNomeTime] = useState('')
-    const [corTime, setCorTime] = useState('')
+  const [nome, setNome] = useState('')
+  const [cargo, setCargo] = useState('')
+  const [imagem, setImagem] = useState('')
+  const [time, setTime] = useState('')
+  const [nomeTime, setNomeTime] = useState('')
+  const [corTime, setCorTime] = useState('')
 
-    const aoSubmeter = (evento) => {
+  const aoSubmeter = (evento) => {
+    evento.preventDefault()
+    console.log('form enviado', nome, cargo, imagem, time)
+    aoCadastrar({
+      nome,
+      cargo,
+      imagem,
+      time
+    })
+  }
+
+  return (
+    <section className="formulario-container">
+      <form className="formulario" onSubmit={aoSubmeter}>
+        <h2>Create New Card</h2>
+        <Campo
+          obrigatorio={true}
+          label='Name'
+          placeholder='Type the name'
+          valor={nome}
+          aoAlterado={valor => setNome(valor)} />
+        <Campo
+          obrigatorio={true}
+          label='Job'
+          placeholder='Type the job'
+          valor={cargo}
+          aoAlterado={valor => setCargo(valor)} />
+        <Campo
+          label='Picture'
+          placeholder='Picture link'
+          aoAlterado={valor => setImagem(valor)} />
+        <ListaSuspensa
+          obrigatorio={true}
+          label='Team'
+          items={times}
+          valor={time}
+          aoAlterado={valor => setTime(valor)} />
+        <Botao texto='Add Card' />
+      </form>
+      <form className="formulario" onSubmit={(evento) => {
         evento.preventDefault()
-        console.log('form enviado', nome, cargo, imagem, time )
-        aoCadastrar({
-            nome,
-            cargo,
-            imagem,
-            time
-        })
-    }
-
-    return (
-        <section className="formulario-container">
-            <form className="formulario" onSubmit={aoSubmeter}>
-                <h2>Create New Card</h2>
-                <Campo
-                    obrigatorio={true}
-                    label='Nome'
-                    placeholder='Digite seu nome '
-                    valor={nome}
-                    aoAlterado={valor => setNome(valor)}/>
-                <Campo
-                    obrigatorio={true}
-                    label='Cargo' 
-                    placeholder='Digite seu cargo '
-                    valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}/>
-                <Campo 
-                    label='Imagem' 
-                    placeholder='Informe o endereço da imagem '
-                    aoAlterado={valor => setImagem(valor)}/>
-                <ListaSuspensa 
-                    obrigatorio={true}
-                    label='Times'
-                    items={times} 
-                    valor={time}
-                    aoAlterado={valor => setTime(valor)}/>
-                <Botao texto='Add Card' />
-            </form>
-            <form className="formulario" onSubmit={(evento) => {
-                evento.preventDefault()
-                cadastrarTime({nome: nomeTime, cor: corTime})
-            }}>
-                <h2>Create New Team</h2>
-                <Campo
-                    obrigatorio
-                    label='Nome'
-                    placeholder='Digite o nome do time'
-                    valor={nomeTime}
-                    aoAlterado={valor => setNomeTime(valor)}
-                />
-                <Campo
-                    obrigatorio
-                    type='color'
-                    label='Cor' 
-                    placeholder='Digite a cor do time'
-                    valor={corTime}
-                    aoAlterado={valor => setCorTime(valor)}
-                />
-                <Botao texto='Add Team'/>
-            </form>
-        </section>
-    )
+        cadastrarTime({ nome: nomeTime, cor: corTime })
+      }}>
+        <h2>Create New Team</h2>
+        <Campo
+          obrigatorio
+          label='Name'
+          placeholder='Team name'
+          valor={nomeTime}
+          aoAlterado={valor => setNomeTime(valor)}
+        />
+        <Campo
+          obrigatorio
+          type='color'
+          label='Color'
+          placeholder='Digite a cor do time'
+          valor={corTime}
+          aoAlterado={valor => setCorTime(valor)}
+        />
+        <Botao texto='Add Team' />
+      </form>
+    </section>
+  )
 }
 
 export default Formulario
